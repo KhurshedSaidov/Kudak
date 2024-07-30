ae6dc9e3f2e5c7d77927dfa804cb11d1475f3bb0
Khurshed Saidov <asgfhsjsks@gmail.com>
2024-07-30 14:49:34 +0500

    1312412

--- cmd/kudak/router.go
+++ cmd/kudak/router.go
@@ -10,5 +10,24 @@ func InitRouters(handler *handlers.Handler) *mux.Router {
 	r.HandleFunc("/login", handler.RegisterHandler).Methods("POST")
 	r.HandleFunc("/signup", handler.SignUpHandler).Methods("POST")
 
+	authRote := r.PathPrefix("/signup").Subrouter()
+
+	authRote.HandleFunc("/kindergarten", handler.CreateKindergartenHandler).Methods("POST")
+	r.HandleFunc("/kindergarten", handler.GetAllKindergartensHandler).Methods("GET")
+	r.HandleFunc("/kindergarten/{id}", handler.GetKindergartenByIDHandler).Methods("GET")
+
+	r.HandleFunc("/education-ministry", handler.CreateEducationMinistryHandler).Methods("POST")
+	r.HandleFunc("/education-ministry/{id:[0-9]+}", handler.GetEducationMinistryByIDHandler).Methods("GET")
+	r.HandleFunc("/education-ministry", handler.GetAllEducationMinistriesHandler).Methods("GET")
+	r.HandleFunc("/education-ministry", handler.UpdateEducationMinistryHandler).Methods("PUT")
+	r.HandleFunc("/education-ministry/{id:[0-9]+}", handler.DeleteEducationMinistryHandler).Methods("DELETE")
+
+	// MainDepartment Routes
+	r.HandleFunc("/main-department", handler.CreateMainDepartmentHandler).Methods("POST")
+	r.HandleFunc("/main-department/{id:[0-9]+}", handler.GetMainDepartmentByIDHandler).Methods("GET")
+	r.HandleFunc("/main-department", handler.GetAllMainDepartmentsHandler).Methods("GET")
+	r.HandleFunc("/main-department", handler.UpdateMainDepartmentHandler).Methods("PUT")
+	r.HandleFunc("/main-department/{id:[0-9]+}", handler.DeleteMainDepartmentHandler).Methods("DELETE")
+
 	return r
 }
